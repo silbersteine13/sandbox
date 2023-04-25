cf17ca26b4b764f66fba70acbc4049b97532a095
silbersteine13 <eric.silberstein@lokalise.com>
2023-04-25 18:02:12 +0000

    Install and configure i18next, add language switcher and resource files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  es: { nativeName: 'Español'}
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button type="submit" key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>{lngs[lng].nativeName}</button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -15,7 +29,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
+          {t('key2')}
         </a>
       </header>
     </div>
